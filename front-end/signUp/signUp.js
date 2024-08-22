f9020b48815c61d1556d5b6faf767f6d46801a3e
jowittaCh <joyamariachahine@gmail.com>
2024-08-22 13:56:44 +0300

    form upgraded

--- front-end/signUp/signUp.js
+++ front-end/signUp/signUp.js
@@ -3,18 +3,19 @@ import { forwardRequest } from "../util/requestSender.js";
 document
   .getElementById("signUpForm")
   .addEventListener("submit", async function (event) {
-    event.preventDefault();
-    console.log("entering my signup.js file");
+    event.preventDefault(); // Prevents the default form submission
 
+    // Create a FormData object from the form element
     const formData = new FormData(event.target);
 
+    // Construct the data object using formData entries
     const data = {
-      firstName:   "dfgd",
-      lastName:    "dfgd",
-      email:       "dfgd",
-      phoneNumber: 66788,
-      password:    "dfgd",
-      dob:         "dfgd",
+      firstName: formData.get("firstName"),
+      lastName: formData.get("lastName"),
+      email: formData.get("email"),
+      phoneNumber: formData.get("phoneNumber"),
+      password: formData.get("password"),
+      dob: formData.get("dob"),
     };
 
     try {
@@ -23,11 +24,14 @@ document
         "POST",
         "http://localhost:3000/user/signUp"
       );
-      if(response.status == 400){
-        alert(response.data.message);
+
+      if (response.status == 400) {
+        alert(response.data.message); //  client-side error
+        if(){
+        }
       }
-      console.log("Success:", response);
+      console.log("Success:", response); // Log the successful response
     } catch (error) {
-      console.error("Error:", error);
-    }
-  });
+      console.error("Error:", error); // Log any errors
+    }
+  });
