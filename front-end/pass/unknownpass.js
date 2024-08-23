7153cc2b7da2b5682cf2053b9dd5f9675624170d
jowittaCh <joyamariachahine@gmail.com>
2024-08-23 21:45:35 +0300

    unkownpass done

--- front-end/pass/unknownpass.js
+++ front-end/pass/unknownpass.js
@@ -8,11 +8,11 @@ document
     const formData = new FormData(event.target);
 
     const data = {
-      email: formData.get("Email"),
+      email: formData.get("email"),
     };
- window.location.href = "./newpass.html"; 
+ 
 
-//    window.location.href = "/front-end/pass/newpass.html";
+//  
     try {
       const response = await forwardRequest(
         data,
@@ -23,9 +23,8 @@ document
     } catch (error) {
       console.error("Error:", error);
     }
-   if (Response.status == 200) {
- window.location.href = "./newpass.html"; 
-    
-   }
+
+      window.location.href = "/front-end/pass/verification.html";
+   
  
   });
