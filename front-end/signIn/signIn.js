d6191fb65fe577f21fd87e3c684aba4ccc6d1f59
jowittaCh <joyamariachahine@gmail.com>
2024-08-23 22:11:42 +0300

    signed in solved

--- front-end/signIn/signIn.js
+++ front-end/signIn/signIn.js
@@ -5,6 +5,7 @@ document
   .addEventListener("submit", async function (event) {
     event.preventDefault();
 
+
     const formData = new FormData(event.target);
 
     const data = {
@@ -19,12 +20,20 @@ document
         "http://localhost:3000/user/login"
       );
 
+     let datamssg =document.getElementById('mssg');
       if (response.status == 400) {
-        alert(response.data.message); //  todo error div
+        datamssg.textContent =response.data.message;
+        //  todo error div
+       /*  window.location.href = "signIn.html"; */
       }
-      // todo 200 successful go to index page
+      else {
       console.log(`user id : ${response._id}`); // to read
-      console.log("Success:", response);
+      console.log("Success:", response); 
+/*       console.log(response.data.message);
+      datamssg.textContent =response.data.message; */
+      sessionStorage.setItem('movieName', 'Inception');
+      window.location.href = "../homePage/index.html";
+    }
     } catch (error) {
       console.error("Error:", error);
     }
