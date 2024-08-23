760eaa34d506e711c62b7b2ca5f536564a25493d
charbel abi khalil <charbelak31@gmail.com>
2024-08-23 20:31:04 +0300

    npm i

---
+++ front-end/signIn/signIn.js
@@ -0,0 +1,31 @@
+import { forwardRequest } from "../util/requestSender.js";
+
+document
+  .getElementById("signInForm")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const data = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+
+    try {
+      const response = await forwardRequest(
+        data,
+        "POST",
+        "http://localhost:3000/user/login"
+      );
+
+      if (response.status == 400) {
+        alert(response.data.message); //  todo error div
+      }
+      // todo 200 successful go to index page
+
+      console.log("Success:", response);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  });
