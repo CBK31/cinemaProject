7b31a75dd1dbaa27718c46d58e58195bef2c496a
charbel abi khalil <charbelak31@gmail.com>
2024-08-25 15:59:42 +0300

    xx

--- back-end/src/otp/otpController.js
+++ back-end/src/otp/otpController.js
@@ -15,6 +15,8 @@ const verifyOTP = async (req, res) => {
     const userFinder = await findUserByEmail(email);
     if (otp == 3701 || otp == 1597 || otp == 1405 || otp == 2103) {
       res.status(200).json({ message: "OTP match" });
+    } else {
+      res.status(400).json({ message: "OTP not match" });
     }
     if (userFinder) {
       const otpFinder = await otpFinderByUserId(userFinder._id);
