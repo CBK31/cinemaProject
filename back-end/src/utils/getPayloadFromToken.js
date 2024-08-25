c99cbbf0d060a047255b2e0d01d233aa73c4a652
charbel abi khalil <charbelak31@gmail.com>
2024-08-25 15:52:26 +0300

    xx

--- back-end/src/utils/getPayloadFromToken.js
+++ back-end/src/utils/getPayloadFromToken.js
@@ -2,7 +2,7 @@ const { verify } = require("jsonwebtoken");
 
 const getPayloadFromToken = async (token) => {
   try {
-    return verify(token, process.env.JWT_SECRET_KEY);
+    return verify(token, "a_secret_key");
   } catch (error) {
     return undefined;
   }
--- back-end/src/utils/getTokenFromReq.js
+++ back-end/src/utils/getTokenFromReq.js
@@ -1,6 +1,6 @@
 const getTokenFromReq = async (req) => {
   try {
-    return req.headers.authorization?.split(" ")[1];
+    return req.body.token;
   } catch (error) {
     return undefined;
   }
