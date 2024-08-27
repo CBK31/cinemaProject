51b7fa713af792e69c6686ec88bd87d7ae355038
jowittaCh <joyamariachahine@gmail.com>
2024-08-27 21:25:49 +0300

    ll

--- front-end/movieInfo/movieinfo.js
+++ front-end/movieInfo/movieinfo.js
@@ -1,13 +1,35 @@
-let movie = JSON.parse(sessionStorage.getItem("movie"));
+ let movie = JSON.parse(sessionStorage.getItem("movie"));
+ console.log(movie);
 
-console.log(movie.show.name);
-alert("hii");
-//   movieId: { type: String },
-//   ended: { type: String },
-//   image: { type: String },
-//   language: { type: String },
-//   name: { type: String },
-//   rating: { type: String },
-//   runtime: { type: String },
-//   summary: { type: String },
-//   genres: { type: [String] },
+function renderMovie(movie) {
+    const movieContainer = document.getElementById('movie-container');
+
+    const movieCard = `
+               <div class="movie-content"> 
+               <div class="movie-image">
+            <img src="${movie.show.image.medium}" alt="${movie.show.name}">
+        </div>
+
+            <div class="movie-info">
+                <h1>${movie.show.name}</h1>
+                <p class="rating"><strong>Rating:</strong> ★ ${movie.show.rating.average}</p>
+                <p class="runtime"><strong>Runtime:</strong> ${movie.show.runtime} min</p>
+                <p class="language"><strong>Language:</strong> ${movie.show.language}</p>
+                <p class="ended"><strong>Date:</strong> ${movie.show.ended}</p>
+                <p class="genres"><strong>Genres:</strong> ${movie.show.genres.join(', ')}</p>
+            </div>
+            </div>
+            
+            <div class="movie-description">
+                <p>${movie.show.summary}</p>
+            
+        </div>
+    `;
+
+    movieContainer.innerHTML = movieCard;
+}
+
+
+renderMovie(movie);
+
+
